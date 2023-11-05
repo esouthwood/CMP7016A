168539c820e6e5c242940cecc2337a041b9905be
Nowshin Ferdaus <77398111+weird-N@users.noreply.github.com>
2023-11-05 09:26:55 +0000

    Add files via upload

---
+++ noisyFiles.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.io import wavfile
+import soundfile as sf
+
+# Reading the noise file and get its data and sampling rate
+noiseFile, fs = sf.read('noise.wav', dtype='float32')
+
+# List of folders containing speech files
+folders = ['ben', 'charlie', 'chiedozie']
+
+
+for folder_name in folders:
+    
+    for file_index in range(20):
+        # Constructing the file path for the current speech file
+        file_name = f'{folder_name}/{file_index}.wav'
+        
+        try:
+            # Reading the speech file and get its data and sampling rate
+            speechFile, fs_speech = sf.read(file_name, dtype='float32')
+            
+            # Ensuring both speech and noise signals have the same length
+            min_length = min(len(speechFile), len(noiseFile))
+            speechFile = speechFile[:min_length]
+            noiseFile = noiseFile[:min_length]
+
+            # Adding noise to speech to create noisy speech
+            noisy_speech = speechFile + noiseFile
+            
+            # Specifing the desired filename for the noisy speech file including folder name
+            noisy_speech_file = f'{folder_name}/{file_index}_noisy_speech.wav'
+            
+            # Saving the noisy speech to a new WAV file
+            wavfile.write(noisy_speech_file, fs_speech, noisy_speech)
+        
+        except Exception as e:
+           
+            print(f'Error processing {file_name}: {e}')
+
