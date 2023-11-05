f3145bf7630caf4cf7afd3adeaf712b86a4be431
esouthwood <uqz19ghu@uea.ac.uk>
2023-11-05 13:26:24 +0000

    Added a dnn
    Used tensorflow.keras to create a deep neural net model

--- split1.py
+++ split1.py
@@ -8,7 +8,10 @@ import os
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, InputLayer
+from tensorflow.keras.optimizers import Adam
 
 folders = ['ben', 'charlie', 'chiedozie', 'carlos', 'el', 'ethan', 'francesca', 'jack', 'jake', 'james',
            'lindon', 'marc', 'nischal', 'robin', 'ryan', 'sam', 'seth', 'william', 'bonney', 'yubo']
@@ -53,3 +56,27 @@ test_size=0.5, random_state=0)
         
             
 
+def create_model():
+    numClasses=20
+    model=Sequential()
+    model.add(InputLayer(input_shape=(280, 12, 1)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Activation('relu'))
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(numClasses))
+    model.add(Activation('softmax'))
+    return model
+
+model = create_model()
+model.compile(loss='categorical_crossentropy',
+              metrics=['accuracy'], optimizer=Adam(learning_rate=0.01))
+model.summary()
+
+num_epochs = 25
+num_batch_size = 32
+
+history = model.fit(X_train, y_train, validation_data=(X_val,y_val), batch_size=num_batch_size, epochs=num_epochs,verbose=1)
+
+model.save_weights('digit_classification.h5')
