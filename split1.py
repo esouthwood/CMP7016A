e9abee262b1fd3199d629b615d4d8ef7fc24f7b1
Nowshin Ferdaus <77398111+weird-N@users.noreply.github.com>
2023-11-06 19:00:30 +0000

    Update split1.py

--- split1.py
+++ split1.py
@@ -13,46 +13,44 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, InputLayer
 from tensorflow.keras.optimizers import Adam
 
+# List of folder names representing different categories or classes
 folders = ['ben', 'charlie', 'chiedozie', 'carlos', 'el', 'ethan', 'francesca', 'jack', 'jake', 'james',
            'lindon', 'marc', 'nischal', 'robin', 'ryan', 'sam', 'seth', 'william', 'bonney', 'yubo']
-data=[]
-labels=[]
 
+# Lists to store extracted features (data) and their corresponding labels
+data = []
+labels = []
+
+# Loop through each folder and process 20 files within each folder
 for folder_name in folders:
-  
     for file_index in range(20):
-        file_name = f'{folder_name}/{file_index}.wav'  
+        # Construct the file path
+        file_name = f'{folder_name}/{file_index}.wav'
       
         try:
-          
-            print(file_name)
+            # Attempt to read audio file and load precomputed MFCC features
             speechFile, fs = sf.read(file_name, dtype='float32')
-            
-            
             mfcc_file = np.load(f'{folder_name}/{file_index}.npy')
-                                   
+            
+            # Append MFCC features to data and corresponding folder name to labels
             data.append(mfcc_file)
             labels.append(folder_name)
     
+            # Print file information
             print(f'Folder: {folder_name}, File: {file_index}, Shape: {speechFile.shape}, Sampling Frequency: {fs}')
         
         except Exception as e:
-          
+            # Handle errors during file processing and print error message
             print(f'Error processing {file_name}: {e}')
-            
-            
-labels = np.array(labels)
-data = np.array(data)   
-from tensorflow.keras.utils import to_categorical
-from sklearn.preprocessing import LabelEncoder
-LE=LabelEncoder()
-labels=to_categorical(LE.fit_transform(labels))
 
-from sklearn.model_selection import train_test_split
-X_train, X_tmp, y_train, y_tmp = train_test_split(data, 
-labels, test_size=0.2, random_state=0)
-X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp,
-test_size=0.5, random_state=0)
+# Converting labels to numpy array and perform label encoding and categorical conversion
+labels = np.array(labels)
+LE = LabelEncoder()
+labels = to_categorical(LE.fit_transform(labels))
+
+# Spliting data into training, validation, and test sets
+X_train, X_tmp, y_train, y_tmp = train_test_split(data, labels, test_size=0.2, random_state=0)
+X_val, X_test, y_val, y_test = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=0)
         
             
 
